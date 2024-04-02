9bfbf51ec35ca9adfb1e1d89181c2f7015226c71
Muzaffer Kılıçaslan <thepitohui@gmail.com>
2024-04-03 02:52:23 +0300

    feat: worked with types

---
+++ src/tutorial.ts
@@ -0,0 +1,35 @@
+// let awesomeName:string = "shakeAndBake";
+// awesomeName = "someting";
+// awesomeName = awesomeName.toUpperCase();
+// console.log(awesomeName);
+
+// //ERROR
+// // awesomeName = 20; 
+
+// let amount:number = 20;
+// amount = amount-2;
+// console.log(amount)
+
+// challenge
+// let isAwesome:boolean= true;
+// isAwesome = !isAwesome;
+// console.log(isAwesome)
+
+// let greeting: string = "Hello, TypeScript!";
+// greeting = greeting.toUpperCase();
+// console.log(greeting);
+
+// let age:number = 25;
+// age = age + 5;
+// console.log(age)
+
+// let isAdult:boolean = age>=18;
+// console.log(isAdult)
+
+// union types
+// let tax: number | string = 10;
+// tax = "20";
+
+// let requestStatus: "pending" | "success" | "error" = "pending";
+// requestStatus = "success";
+
